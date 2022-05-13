3e9be56cabe54f81c4c6593d8ddaebb4a311a5fb
ハチ <82988679+pichayakorn@users.noreply.github.com>
2022-05-13 15:56:23 +0700

    Initial commit

---
+++ src/components/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <card class="center"></card>
+    <card class="center">2nd Card</card>
+    <card class="center"
+      ><card style="width: 200px; margin: 0 auto 0 auto">3rd Card</card></card
+    >
+  </div>
+</template>
+
+<script>
+import card from "./card";
+
+export default {
+  components: {
+    card,
+  },
+};
+</script>
+
+<style>
+.center {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ src/components/vbind.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>V-Bind</h1>
+    <button
+      v-bind:class="{ green: check, red: !check }"
+      v-on:click="check = !check"
+    />
+    <p v-if="check">This is TRUE</p>
+    <p v-else>This is FALSE</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      check: true,
+    };
+  },
+};
+</script>
+
+<style>
+.green {
+  background-color: green;
+  width: 100px;
+  height: 100px;
+}
+.red {
+  background-color: red;
+  width: 100px;
+  height: 100px;
+}
+</style>
